f12ccb53dee2d11deb1fb8b7584c21319af09345
Айгуль Каримова <karimova2493@mail.ru>
2016-03-31 02:41:27 +0300

    task01 is done

---
+++ HomeworkJava/src/lesson29032016/Task01.java
@@ -0,0 +1,10 @@
+package lesson29032016;
+
+public class Task01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println(1+2+3+4+5+6+7+8+9+10);
+	}
+
+}
